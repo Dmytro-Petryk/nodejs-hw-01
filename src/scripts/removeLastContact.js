17cb564a795868da02328c54726861c42d0b29e8
Dima <dimapetrik416@gmail.com>
2025-07-26 16:56:12 -0500

    1.0

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,20 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    if (contacts.length === 0) {
+      console.log('No contacts to remove.');
+      return;
+    }
+
+    contacts.pop();
+    await writeContacts(contacts);
+    console.log('Last contact removed successfully.');
+  } catch (error) {
+    console.error('Failed to remove last contact:', error);
+  }
+};
 
 removeLastContact();
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,11 @@
+import { writeFile } from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (contacts) => {
+  try {
+    await writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+  } catch (error) {
+    console.error('Failed to write contacts:', error);
+    throw error;
+  }
+};
